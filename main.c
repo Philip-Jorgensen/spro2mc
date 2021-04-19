d064326789fee4d522458aa53e9fce32605ebe2c
Philip-Jorgensen <79910746+Philip-Jorgensen@users.noreply.github.com>
2021-04-19 21:47:41 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+SPRO2MC
+main.c
+2021
+
+Remember to add proper comments and explanations when you make changes.
+*/
+
+// Function Prototypes
+
+int main(void){
+  while(1){
+    // Enter your code here.
+  }
+}
