6ae4aed4a3ff56943c1bd2b741537940a610e340
Philip-Jorgensen <79910746+Philip-Jorgensen@users.noreply.github.com>
2021-04-19 22:00:49 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,15 @@ main.c
 Remember to add proper comments and explanations when you make changes.
 */
 
+// Definitions
+#define F_CPU 16E6
+
+// Imports
+#import <avr/io.h>
+#import <stdio.h>
+#import <stdlib.h>
+#import "usart.h"
+
 // Function Prototypes
 
 int main(void){
