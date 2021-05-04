e0ddad060029296d1a5e377cb2ac625929f3b7c8
Philip <phjoe20@student.sdu.dk>
2021-05-04 13:47:53 +0200

    Motor driver (testing)

---
+++ testing/Motor_driver/Motor_driver/main.c
@@ -0,0 +1,18 @@
+/*
+ * Motor_driver.c
+ *
+ * Created: 5/4/2021 1:47:22 PM
+ * Author : Philip J
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
