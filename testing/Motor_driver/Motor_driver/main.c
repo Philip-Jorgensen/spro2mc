d63ba5a2c822646fb63961c66055aef99bd23ae9
Philip <phjoe20@student.sdu.dk>
2021-05-04 14:11:00 +0200

    drive the motor 1

--- testing/Motor_driver/Motor_driver/main.c
+++ testing/Motor_driver/Motor_driver/main.c
@@ -6,13 +6,28 @@
  */ 
 
 #include <avr/io.h>
+#include <stdio.h>
+#include "PCA9685_ext.c"
+#include "twimaster.c"
 
 
 int main(void)
 {
-    /* Replace with your application code */
+    motor_init_pwm(PWM_FREQUENCY_1500);
+	
+	// Making sure that all the motors are stopped when it starts.
+	motor_set_state(M1, STOP);
+	motor_set_state(M2, STOP);
+	motor_set_state(M3, STOP);
+	motor_set_state(M4, STOP);
+	
+	// Starting the first motor
+	motor_set_state(M1, CCW); // CCCW -> Counter clockwise
+	motor_set_pwm(M1, 0, 0x1ff); // PWM: stop = 0, start = 511 (0x1ff)
+	
     while (1) 
     {
+		
     }
 }
 
