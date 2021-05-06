e64c94f02c53785cbf2cb1f2ae9d09f2ad697cc3
Philip <phjoe20@student.sdu.dk>
2021-05-06 12:38:36 +0200

    The motor runs

--- testing/Motor_driver/Motor_driver/main.c
+++ testing/Motor_driver/Motor_driver/main.c
@@ -5,30 +5,43 @@
  * Author : Philip J
  */ 
 
+#define F_CPU 16000000UL
+
 #include <avr/io.h>
 #include <stdio.h>
 #include "PCA9685_ext.h"
 #include "i2cmaster.h"
+#include "usart.h"
+#include <util/delay.h>
+
 void control_motor(unsigned char , unsigned char , unsigned int , unsigned int );
 
 int main(void)
 {
-    motor_init_pwm(PWM_FREQUENCY_1500);
 	
-	// Making sure that all the motors are stopped when it starts.
+	uart_init(); // Open the communication to the micro controller
+	i2c_init(); // Initialize the i2c communication.
+	io_redirect(); // Redirect the input/output to the computer.
+	
+	// Make sure all the motors are stopped from the beginning (Initialization)
+	motor_init_pwm(PWM_FREQUENCY_200);
+	
+	printf("Adafruit 1438");
+	
+	// M1,..,M4 are ports on the "Adafruit 1438"
 	motor_set_state(M1, STOP);
 	motor_set_state(M2, STOP);
 	motor_set_state(M3, STOP);
 	motor_set_state(M4, STOP);
 	
-	// Starting the first motor
-	motor_set_state(M1, CCW); // CCCW -> Counter clockwise
-	motor_set_pwm(M1, 0, 0x1ff); // PWM: stop = 0, start = 511 (0x1ff)
+	/* This piece of code does make the motor turn, but very slow.
+	motor_set_state(M1, CW);
+	motor_set_pwm(M1, 0, 0x1ff);
+	*/
 	
-    while (1) 
-    {
+	while(1){
 		
-    }
+	}
 }
 void control_motor(unsigned char motor_id, unsigned char state, unsigned int on_value, unsigned int off_value){
 	motor_set_state(motor_id, state);
