030ec8b2f735b509a17b6dd0e81fad7bc4152284
fzemlin <frzem20@student.sdu.dk>
2021-05-11 12:13:08 +0200

    setup of functions
    I made some functions for sensor etc. ,these are not complete, except for the controlMotor one. We need to do further testing to finalize the functions. The code in main for the movement will follow the flowchart we have made for the continues brachiation.

--- functions/functions.c
+++ functions/functions.c
@@ -1,4 +1,54 @@
 // Include libraries here
 #include "functions.h"
+#include <util/delay.h>
 
 // Function definitions below.
+void closeGrabbers(){
+	//rotate grabbers
+	_delay_ms(10);
+	//PORTD|=1<<PIND4;//lock grabbers
+	
+}
+void openGrabbers(){
+	//PORTD&=~(1<<PIND4);//unlock grabbers
+	_delay_ms(10)
+	//rotate grabbers to open
+}
+double detectBarGrabbers(){
+	double distanceToBar;
+	//read the sensors for the grabbers
+	return distanceToBar;
+}
+void moveMotor(unsigned char motor_id, int on_value, int time_on){
+	 control_motor(unsigned char motor_id, int on_value)
+	 _delay_ms(time_on);//we probably have throw the delay out
+	 motor_set_pwm(motor_id,0,0);
+}
+void control_motor(unsigned char motor_id, int on_value){
+	if(on_value>=0){ // If the run value (speed?) is greater than 0, make it run clockwise.
+		motor_set_state(motor_id,CW);
+		motor_set_pwm(motor_id,on_value,0);
+	}
+	if(on_value<0){
+		motor_set_state(motor_id,CCW); // If speed is less than 0, make it run counter clockwise.
+		motor_set_pwm(motor_id,(-1)*on_value,0); // Since 'on_value' is below 0, it is multiplied by (-1) to make it positive.
+	}
+}
+double readUltrasonic(){
+	double distance;
+	//when the test code works copy it in here
+	return distance;
+}
+double readAccleration(char axis){//axis is 'y'or'x'or'z'
+	double acceleration;
+	if (axis=='x')
+	{
+	}
+	if (axis=='y')
+	{
+	}
+	if (axis=='z')
+	{
+	}
+	return acceleration;
+}
--- main.c
+++ main.c
@@ -14,13 +14,56 @@ Remember to add proper comments and explanations when you make changes.
 #include <stdio.h>
 #include <stdlib.h>
 #include "usart.h"
+#include "PCA9685_ext"
 
 #include "functions/functions.h"
 
 // Function Prototypes
+void closeGrabbers(void);
+void openGrabbers(void);
+void control_motor(unsigned char, int);
+double detectBarGrabbers(void);
+double readUltrasonic(void);
+void moveMotor(unsigned char, int, int);
+double readAccleration(char);
+
+struct Motors 
+{
+	unsigned char GR_Elbow_L;//motor ids for the equivalent motors in the simulation
+	unsigned char PR_Elbow_L;
+	unsigned char GR_Shoulder_L;
+	unsigned char PR_Shoulder_L;
+	unsigned char GR_Grabber;
+	unsigned char PR_Grabber;
+	unsigned char GR_Elbow_R;
+	unsigned char PR_Elbow_R;
+	unsigned char GR_Shoulder_R;
+	unsigned char PR_Shoulder_R;
+};
 
 int main(void){
+	
+	uart_init(); // Open the communication to the micro controller
+	i2c_init(); // Initialize the i2c communication.
+	io_redirect(); // Redirect the input/output to the computer.
+	
+	struct Motors motors;
+	
+	// Make sure all the motors are stopped from the beginning (Initialization)
+	motor_init_pwm(PWM_FREQUENCY_1500);
+	
+	printf("Adafruit 1438\n");
+	
+	// M1,..,M4 are ports on the "Adafruit 1438"
+	motor_set_state(M1, STOP);
+	motor_set_state(M2, STOP);
+	motor_set_state(M3, STOP);
+	motor_set_state(M4, STOP);
+	motor_set_state(M5, STOP);
+	motor_set_state(M6, STOP);
+	motor_set_state(M7, STOP);
   while(1){
-    // Enter your code here.
+    //start with the movement
+	moveMotor(motors.GR_Elbow_L,-50,500);//example move the left green elbow motor counterclockwise with signal of 50 for 500ms
   }
 }
