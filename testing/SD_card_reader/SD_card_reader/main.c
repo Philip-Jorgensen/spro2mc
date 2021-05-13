c9d618c62b2ea90a2a0dc9be4f740f2199acad4e
Philip <phjoe20@student.sdu.dk>
2021-05-13 13:32:35 +0200

    SD Card reader

---
+++ testing/SD_card_reader/SD_card_reader/main.c
@@ -0,0 +1,18 @@
+/*
+ * SD_card_reader.c
+ *
+ * Created: 5/13/2021 1:27:47 PM
+ * Author : Philip J
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
