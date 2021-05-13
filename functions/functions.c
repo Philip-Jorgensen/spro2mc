82686d87f5d004c570bc133256477718ee96915e
fzemlin <frzem20@student.sdu.dk>
2021-05-13 10:29:13 +0200

    proximity switch function + fixes
    I added the proximity switch function and fixed some errors

--- functions/functions.c
+++ functions/functions.c
@@ -1,6 +1,16 @@
 // Include libraries here
-#include "functions.h"
+#define F_CPU 16000000UL
+
+// Imports
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <util/delay.h>
+#include "usart.h"
+#include "PCA9685_ext.h"
+#include "i2cmaster.h"
+#include "functions.h"
 #include "PCA9685_ext.h"
 
 // Function definitions below.
@@ -24,7 +34,7 @@ void closeGrabbers(unsigned char motor_id){
 void openGrabbers(unsigned char motor_id){
 	//PORTD&=~(1<<PIND4);//unlock the solenoid.
 	
-	_delay_ms(10)
+	_delay_ms(10);
 	//rotate grabbers
 	control_motor(motor_id, 1);
 	_delay_ms(130);
@@ -35,15 +45,18 @@ void openGrabbers(unsigned char motor_id){
 	
 	// Lock the solenoid.
 }
-double distanceBarGrabbers(){
-	double distanceToBar;
-	//read the sensors for the grabbers
-	return distanceToBar;
+int BarDetected(){//proximity switch function
+	int bar_detected;
+	if ((PINB&(1<<PINB1))==1)//we might want to change the pin on the arduino
+		bar_detected=1;
+	else
+		bar_detected=0;
+	return bar_detected;
 }
 
 // Only moves the motor for a given time, 'time_on', and then stops the motor again.
 void moveMotor(unsigned char motor_id, int on_value, int time_on){
-	 control_motor(unsigned char motor_id, int on_value)
+	 control_motor(motor_id, on_value);
 	 _delay_ms(time_on);//we probably have to throw the delay out
 	 motor_set_pwm(motor_id,0,0);
 }
@@ -57,13 +70,13 @@ void control_motor(unsigned char motor_id, int on_value){
 		motor_set_pwm(motor_id,(-1)*on_value,0); // Since 'on_value' is below 0, it is multiplied by (-1) to make it positive.
 	}
 }
-double readUltrasonic(){
+double readUltrasonic(double pulse){
 	double distance=0;
 	distance=((double)pulse)*0.0000000625*342.2/2;//pulse*time for one tick (1/16mhz)*speed of sound(20C)/2 
 	return distance;
 }
 double readAccleration(char axis){//axis is 'y'or'x'or'z'
-	double acceleration;
+	double acceleration=0;
 	if (axis=='x')
 	{
 		// Reading the acceleration in the x direction.
--- functions/functions.h
+++ functions/functions.h
@@ -6,9 +6,9 @@ void closeGrabbers(unsigned char);
 void openGrabbers(unsigned char);
 void control_motor(unsigned char, int);
 double distanceBarGrabbers(void);
-double readUltrasonic(void);
+double readUltrasonic(double);
 void moveMotor(unsigned char, int, int);
 double readAccleration(char);
-double readUltrasonic(void);
+int BarDetected(void);
 
 #endif
