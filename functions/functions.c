7f7f3434e856eace5b2bc1481326b5f96910885b
Philip <79910746+Philip-Jorgensen@users.noreply.github.com>
2021-05-13 10:33:45 +0200

    Added conv_j30
    conv_j30 is a function that converts RPS into a speed value the control_motor function can use.

--- functions/functions.c
+++ functions/functions.c
@@ -1,16 +1,6 @@
 // Include libraries here
-#define F_CPU 16000000UL
-
-// Imports
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <util/delay.h>
-#include "usart.h"
-#include "PCA9685_ext.h"
-#include "i2cmaster.h"
 #include "functions.h"
+#include <util/delay.h>
 #include "PCA9685_ext.h"
 
 // Function definitions below.
@@ -34,7 +24,7 @@ void closeGrabbers(unsigned char motor_id){
 void openGrabbers(unsigned char motor_id){
 	//PORTD&=~(1<<PIND4);//unlock the solenoid.
 	
-	_delay_ms(10);
+	_delay_ms(10)
 	//rotate grabbers
 	control_motor(motor_id, 1);
 	_delay_ms(130);
@@ -45,18 +35,15 @@ void openGrabbers(unsigned char motor_id){
 	
 	// Lock the solenoid.
 }
-int BarDetected(){//proximity switch function
-	int bar_detected;
-	if ((PINB&(1<<PINB1))==1)//we might want to change the pin on the arduino
-		bar_detected=1;
-	else
-		bar_detected=0;
-	return bar_detected;
+double distanceBarGrabbers(){
+	double distanceToBar;
+	//read the sensors for the grabbers
+	return distanceToBar;
 }
 
 // Only moves the motor for a given time, 'time_on', and then stops the motor again.
 void moveMotor(unsigned char motor_id, int on_value, int time_on){
-	 control_motor(motor_id, on_value);
+	 control_motor(unsigned char motor_id, int on_value)
 	 _delay_ms(time_on);//we probably have to throw the delay out
 	 motor_set_pwm(motor_id,0,0);
 }
@@ -70,13 +57,13 @@ void control_motor(unsigned char motor_id, int on_value){
 		motor_set_pwm(motor_id,(-1)*on_value,0); // Since 'on_value' is below 0, it is multiplied by (-1) to make it positive.
 	}
 }
-double readUltrasonic(double pulse){
+double readUltrasonic(){
 	double distance=0;
 	distance=((double)pulse)*0.0000000625*342.2/2;//pulse*time for one tick (1/16mhz)*speed of sound(20C)/2 
 	return distance;
 }
 double readAccleration(char axis){//axis is 'y'or'x'or'z'
-	double acceleration=0;
+	double acceleration;
 	if (axis=='x')
 	{
 		// Reading the acceleration in the x direction.
@@ -93,4 +80,13 @@ double readAccleration(char axis){//axis is 'y'or'x'or'z'
 		
 	}
 	return acceleration;
+}
+// A function for converting rps to the speed value the motor library needs.
+// This is for the 30 RPM Joint motor.
+int conv_j30(double rps){
+	if(x>0){
+		// This function can be found in notion.
+		return (int)(-7930*x+4090);
+	}
+	else{return 0;} // The function only works when it's a positive number (not 0).
 }
