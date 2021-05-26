cdfe5c73666464ecafd0cdbba5c87d50ff77d4b8
Philip <79910746+Philip-Jorgensen@users.noreply.github.com>
2021-05-26 11:09:47 +0200

    Update functions.h

--- Main/Main/functions.h
+++ Main/Main/functions.h
@@ -13,6 +13,12 @@ double readAccleration(char);
 int BarDetected(void);
 int rps_to_speedValue(double); // A function to convert rps to speed for the code. (Joint motor)
 
+// Definitions
+
+#define JOINT_MOTOR 0; // 30RPS Worm gear motor
+
+#define SWING_TIME 1000; // Time between each swing in the continuous brachiation
+
 // Motor IDs
 #define G_Grabbers 1;
 #define G_Elbows 2;
